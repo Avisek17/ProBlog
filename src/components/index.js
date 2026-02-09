68b445afbcc36cb112dffe2955e0b133d0b7f27d
Avisek <[avisekdulal07@gmail.com]>
2026-02-09 23:40:48 +0545

    editor and post form done

--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,13 @@ import Logo from "./Logo";
 import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button"
+import RTE from "./RTE";
 export {
     Header,
     Footer,
     Logo,
     Container,
     LogoutBtn,
-    Button
+    Button,
+    RTE
 }
