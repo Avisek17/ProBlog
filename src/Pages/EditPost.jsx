de5fc0e2b991f7863ee0122049f601cc516e8c7d
Avisek <[avisekdulal07@gmail.com]>
2026-02-10 23:04:50 +0545

    Editpost page

--- src/Pages/EditPost.jsx
+++ src/Pages/EditPost.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Container, PostForm } from '../components'
+import appwriteService from '../appwrite/config'
 
 function EditPost() {
-  return (
-    <div>EditPost</div>
-  )
+    const [post, setPost] = useState()
+    const {slug } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(slug){
+            appwriteService.getPost(slug).then((post) =>{
+                if(post){
+                    setPost(post)
+                }
+            })
+        }else{
+            navigate('/')
+        }
+    }, [slug, navigate])
+  return post ? (
+
+    <div className='py-8'>
+    <Container>
+        <PostForm post={post} />
+    </Container>
+    </div>
+  ) : null
 }
 
 export default EditPost
